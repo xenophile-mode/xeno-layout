036fb098ec5b52b99a7449e9ffc5a7255890ecac
xenophile-mode <erickgpersonalacct1@gmail.com>
2021-05-02 17:38:31 -0500

    updated layout

--- keymap.c
+++ keymap.c
@@ -41,9 +41,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = LAYOUT_ergodox_pretty(
   // left hand
   KC_EQL,          KC_1,        KC_2,          KC_3,    KC_4,    KC_5,    KC_LEFT,              KC_RGHT,      KC_6,    KC_7,    KC_8,    KC_9,              KC_0,           KC_MINS,
-  KC_DEL,          KC_Q,        KC_W,          KC_E,    KC_R,    KC_T,    KC_NO,                KC_NO,     KC_Y,    KC_U,    KC_I,    KC_O,              KC_P,           KC_BSLS,
+  KC_TAB,          KC_Q,        KC_W,          KC_E,    KC_R,    KC_T,    KC_NO,                KC_NO,     KC_Y,    KC_U,    KC_I,    KC_O,              KC_P,           KC_BSLS,
   KC_P,         KC_A,        KC_S,          KC_D,    KC_F,    KC_G,                                        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, GUI_T(KC_QUOT),
-  KC_LSFT,         CTL_T(KC_Z), KC_X,          KC_C,    KC_V,    KC_B,    KC_NO,                      KC_ESC, KC_N,    KC_M,    KC_COMM, KC_DOT,           KC_SLSH, KC_RSFT,
+  KC_LSFT,         CTL_T(KC_Z), KC_X,          KC_C,    KC_V,    KC_B,    KC_B,                      KC_ESC, KC_N,    KC_M,    KC_COMM, KC_DOT,           KC_SLSH, KC_RSFT,
   KC_GRV, KC_QUOT,     LALT(KC_LSFT), KC_LEFT, KC_RGHT,                                              KC_UP,   KC_DOWN, KC_LBRC, KC_RBRC, MEH_T(KC_NO),
                                                            KC_DEL, KC_COPY,                KC_PSTE, CTL_T(KC_ESC),
                                                                           KC_HOME,                 KC_PGUP,
